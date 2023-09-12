fa90998022e451b70c1ad7c95134b2a32196a654
coss-fz <julicofito@gmail.com>
2023-09-11 19:31:02 -0500

    neural network class working properly

--- src/neural_network.py
+++ src/neural_network.py
@@ -14,7 +14,14 @@ import random
 
 
 
-class RNA:
+class Artificial_Neural_Network:
+  """
+  This class is a personalized MLP, it has one neuron per output and the
+  class prediction always depends on the calculation of the sigmoidal activation function
+
+  This MLP class is not optimized, it has a considerably long execution time, keep in
+  mind that this code is for educational purposes, not productive
+  """
 
   AJ = None
   AK = None
@@ -36,73 +43,86 @@ class RNA:
       self.__class__.HJ = np.zeros((self.hidden_size+1,1))
 
 
-  #######Método de Instancia#######
+  ####### Instance Method #######
   def graph_neural_network(self) -> None:
+    """
+    This function makes a graph with the neural network topology
+      input layer -> hidden layer -> output layer
+    """
 
-      total_height = self.input_size + max(self.hidden_size, self.output_size) #Calcular la altura total de la gráfica
-      center_y = total_height / 2 #Calcular la posición vertical del centro de la gráfica
-      fig = plt.figure(figsize=(10, 5)) #Crear una figura y definir el tamaño
+    total_height = self.input_size + max(self.hidden_size, self.output_size) #Calculate the total height of the graphic
+    center_y = total_height / 2 #Calculate the vertical position of the center of the graphic
+    fig = plt.figure(figsize=(10, 5))
 
-      ##Agregar los nodos de la primera capa
-      for i in range(self.input_size):
-          plt.scatter(0, i+0.5+(center_y-self.input_size/2), s=500, c='black')
-          
-      ##Agregar los nodos de la segunda capa
-      for i in range(self.hidden_size):
-          plt.scatter(2, i+0.5+(center_y-self.hidden_size/2), s=500, c='black')
-          
-          ##Conectar los nodos de la primera y segunda capa
-          for j in range(self.input_size):
-              plt.plot([0, 2], [j+0.5+(center_y-self.input_size/2), i+0.5+(center_y-self.hidden_size/2)], c='black')
-              
-      ##Agregar los nodos de la tercera capa
-      for i in range(self.output_size):
-          plt.scatter(4, i+0.5+(center_y-self.output_size/2), s=500, c='black')
-          
-          ##Conectar los nodos de la segunda y tercera capa
-          for j in range(self.hidden_size):
-              plt.plot([2, 4], [j+0.5+(center_y-self.hidden_size/2), i+0.5+(center_y-self.output_size/2)], c='black')
+    ##Add the nodes of the input layer
+    for i in range(self.input_size):
+        plt.scatter(0, i+0.5+(center_y-self.input_size/2), s=500, c='black')
+        
+    ##Add the nodes of the hidden layer
+    for i in range(self.hidden_size):
+        plt.scatter(2, i+0.5+(center_y-self.hidden_size/2), s=500, c='black')
+        
+        ##Connect the nodes
+        for j in range(self.input_size):
+            plt.plot([0, 2], [j+0.5+(center_y-self.input_size/2), i+0.5+(center_y-self.hidden_size/2)], c='black')
+            
+    ##Add the nodes of the output layer
+    for i in range(self.output_size):
+        plt.scatter(4, i+0.5+(center_y-self.output_size/2), s=500, c='black')
+        
+        ##Connect the nodes
+        for j in range(self.hidden_size):
+            plt.plot([2, 4], [j+0.5+(center_y-self.hidden_size/2), i+0.5+(center_y-self.output_size/2)], c='black')
 
-      ##Desactivar los ejes
-      plt.axis('off')
-
-      ##Mostrar la figura
-      plt.show()
+    plt.axis('off')
+    plt.show()
     
     
-  #######Método de Instancia#######
+  ####### Instance Method #######
   def fit(self,
           features:np.ndarray, 
           labels:np.ndarray,
-          hidden:int, 
           alpha:float, 
           iterations:int, 
-          ECM_stop:float) -> np.ndarray and np.ndarray and np.ndarray: 
+          ECM_stop:float) -> np.ndarray and np.ndarray and np.ndarray:
+    """
+    This function is in charge to perform the training of the neural network
+      Parameters:
+        features -> array with the dataset features
+        labels -> array with the dataset class labels
+        alpha -> learning rate
+        iterations -> training repetitions
+        ECM_stop -> target loss to stop training
+      Returns:
+        YK_T -> training predictions
+        w -> hidden layer weights
+        c -> output layer weights
+    """
 
     rows = features.shape[0]
     inputs = self.input_size
+    hidden = self.hidden_size
     outputs = self.output_size
 
-    ##Agregación Capa Oculta
+    ##Agregations
     AJ = self.__class__.AJ
     AK = self.__class__.AK 
 
-    ##Funciones de Activación de la Salida
+    ##Activations
     HJ = self.__class__.HJ 
-    ####HJ[hidden] = 1 #Agregar 'BIAS'
-    
+    ####HJ[hidden] = 1 #Add 'BIAS'
     YK_T = np.zeros((rows,outputs))
 
-    a = alpha #Factor de Aprendizaje
+    a = alpha #Definition of the Learning Rate
 
-    ##Pesos Neuronales
+    ##Neural Weights
     w = np.random.rand(hidden,inputs)
     c = np.random.rand(outputs, hidden+1)
 
-    YD_T = labels #Valor Deseado
+    YD_T = labels #Real Value
     YD_T = YD_T.reshape((YD_T.shape[0], 1))
 
-    ##Errores
+    ##Losses
     E = np.zeros((rows,outputs))
     ECM = np.zeros((outputs,1))
     ECM_T = np.zeros((outputs,1))
@@ -112,40 +132,40 @@ class RNA:
     for iter in range(max_iter):
 
       if (iter+1 == max_iter):
-        print("\nSE LLEGÓ AL LÍMITE DE ITERACIONES\n")
+        print("\nITERATIONS LIMIT REACHED\n")
         break
 
       ECM[:] = 0
-      SUMA_ECM = 0
+      ECM_SUM = 0
       
       for i in range(0, len(features)):
-        ##Agregación de Capa Oculta
+        ##Hidden Layer Agregation
         AJ[:] = 0
         for k in range(hidden):
           for j in range(0, len(w[0])):
-            AJ[k] = AJ[k] +features[i][j]*w[k][j]
-        ##Función de Activación de Capa Oculta
+            AJ[k] = AJ[k]+features[i][j]*w[k][j]
+        ##Hidden Layer Activation Function
         for j in range(hidden):
           HJ[j] = 1/(1+exp(-AJ[j]))
-        ##Agregación de Capa de Salida
+        ##Output Layer Agregation
         AK[:] = 0
         for k in range(outputs):
           for j in range(0, len(c[0])):
             AK[k] = AK[k]+HJ[j]*c[k][j]
-        ##Función de Activación de Capa de Salida
+        ##Output Layer Activation Function
         for k in range(outputs):
           YK_T[i][k] = 1/(1+exp(-AK[k])) 
         
-        ##Error
+        ##Loss
         for k in range(outputs):
           E[i][k] = YD_T[i][k]-YK_T[i][k]
           
-        ##Entrenamiento de Capa de Salida
+        ##Output Layer Training
         for k in range(outputs):
           for j in range(len(c[0])):
             ####c[k][j] = c[k][j]+alfa*E[i][k]*1*HJ[j]
             c[k][j] = c[k][j]+a*E[i][k]*(YK_T[i][k]*(1-YK_T[i][k]))*HJ[j]
-        ##Entrenamiento de Capa Oculta
+        ##Hidden Layer Training
         for e in range(outputs):
           for k in range(hidden):
             for j in range(len(w[0])):
@@ -158,13 +178,13 @@ class RNA:
           ECM[k] = ECM[k]+((E[i][k]**2/2))
 
       ECM_T = np.hstack((ECM_T, ECM))
-      SUMA_ECM = np.sum(ECM)
+      ECM_SUM = np.sum(ECM)
 
-      if SUMA_ECM <= ECM_stop:
+      if ECM_SUM <= ECM_stop:
         break
 
-    ##Gráfica ECM
-    print("ECM = ", SUMA_ECM)
+    ##ECM Graphic
+    print("ECM = ", ECM_SUM)
     plt.figure(figsize=(10,5))
     plt.plot(ECM_T[0], 'k')
     plt.title('ECM Entrenamiento')
@@ -173,74 +193,97 @@ class RNA:
     return YK_T, w, c
 
 
-  #######Método de Instancia#######
+  ####### Instance Method #######
   def predict(self,
               features:np.ndarray,
               w:np.ndarray, 
               c:np.ndarray) -> np.ndarray:
+    """
+    This functions performs the prediction of the trained MLP model
+      Parameters:
+        features -> array with the dataset features
+        w -> hidden layer wieghts
+        c -> output layer weights
+      Returns:
+        YK_V -> array with the predicted labels
+    """
 
-      filas = features.shape[0]
-      salidas = self.output_size
-      hidden = self.hidden_size
+    rows = features.shape[0]
+    outputs = self.output_size
+    hidden = self.hidden_size
 
-      ##Agregación Capa Oculta
-      AJ = self.__class__.AJ  
-      AK = self.__class__.AK  
+    ##Agregations
+    AJ = self.__class__.AJ  
+    AK = self.__class__.AK  
 
-      ##Funciones de Activación de la Salida
-      HJ = self.__class__.HJ 
-      ####HJ[hidden] = 1 #Agregar 'BIAS'
+    ##Activations
+    HJ = self.__class__.HJ 
+    ####HJ[hidden] = 1 #Add 'BIAS'
+    YK_V = np.zeros((rows,outputs))
 
-      YK_V = np.zeros((filas,salidas))
-
-      for i in range(0, features.shape[0]):
-          ##Agregación Capa Oculta
-          AJ[:] = 0
-          for k in range(hidden):
-              for j in range(0, len(w[0])):
-                  AJ[k] = AJ[k]+features[i][j]*w[k][j]
-          ##Función de Activación de Capa Oculta
-          for j in range(hidden):
-              HJ[j] = 1/(1+np.exp(-AJ[j]))
-          ##Agregación de Capa de Salida
-          AK[:] = 0
-          for k in range(salidas):
-              for j in range(0, len(c[0])):
-                  AK[k] = AK[k]+HJ[j]*c[k][j]
-          ##Función de Activación de Capa de Salida
-          for k in range(salidas):
-              YK_V[i][k] = 1/(1+exp(-AK[k])) 
+    for i in range(0, features.shape[0]):
+        ##Hidden Layer Agregation
+        AJ[:] = 0
+        for k in range(hidden):
+            for j in range(0, len(w[0])):
+                AJ[k] = AJ[k]+features[i][j]*w[k][j]
+        ##Hidden Layer Activation Function
+        for j in range(hidden):
+            HJ[j] = 1/(1+np.exp(-AJ[j]))
+        ##Output Layer Agregation
+        AK[:] = 0
+        for k in range(outputs):
+            for j in range(0, len(c[0])):
+                AK[k] = AK[k]+HJ[j]*c[k][j]
+        ##Output Layer Activation Function
+        for k in range(outputs):
+            YK_V[i][k] = 1/(1+exp(-AK[k])) 
 
 
-      return YK_V
+    return YK_V
 
 
   @staticmethod
   def accuracy(true_values:np.ndarray, 
                predicted_values:np.ndarray, 
                scaler:sklearn.preprocessing._data.MinMaxScaler) -> float:
+    """
+    This function calculates the general accuracy of the MLP model
+      Parameters:
+        true_values -> aray with the real values
+        predicted_values -> array with the model predictions
+        scaler -> Scaler object that was used in the data preprocessing
+      Returns:
+        accuracy_score -> general accuracy
+    """
 
-      n_partitions = len(np.unique(true_values)) #Definir la cantidad de particiones
-      bins = np.linspace(0, 1, n_partitions + 1) #Crear los límites de los rangos
-      arr_class = np.digitize(predicted_values, bins).astype(float) #Clasificar los datos en los rangos
-      arr_class = (arr_class - 1) / (n_partitions - 1) #Asignar la clase correspondiente
+    n_partitions = len(np.unique(true_values)) #Define the number of partitions
+    bins = np.linspace(0, 1, n_partitions + 1) #Create ranges' limits
+    arr_class = np.digitize(predicted_values, bins).astype(float) #Classify the data in the ranges
+    arr_class = (arr_class - 1) / (n_partitions - 1) #Asign the corresponding class
 
 
-      return accuracy_score(scaler.inverse_transform(true_values.reshape((true_values.shape[0], 1))), 
-                            scaler.inverse_transform(arr_class))
+    return accuracy_score(scaler.inverse_transform(true_values.reshape((true_values.shape[0], 1))), 
+                          scaler.inverse_transform(arr_class))
 
 
-  #######Método de Instancia#######
+  ####### Instance Method #######
   def graph_results(self,
                     true_values:np.ndarray, 
                     predicted_values:np.ndarray) -> None:
+    """
+    This function makes a graph with the prediction results of the MLP model
+      Parameters:
+        true_values -> array with the real values
+        predicted_values -> array with the model predictions
+    """
     
     outputs = self.output_size
 
-    n_partitions = len(np.unique(true_values)) #Definir la cantidad de particiones
-    bins = np.linspace(0, 1, n_partitions + 1) #Crear los límites de los rangos
+    n_partitions = len(np.unique(true_values)) #Define the number of partitions
+    bins = np.linspace(0, 1, n_partitions + 1) #Create ranges' limits
 
-    ##Deseado VS Respuesta
+    ##Real VS Prediction
     aux_YD = true_values
     aux_YK = np.zeros((outputs, len(true_values)))
     for i in range(outputs):
@@ -250,32 +293,41 @@ class RNA:
     plt.figure(figsize=(10,5))
     for i in range(len(bins)):
       plt.plot(np.array(range(len(aux_YD))),np.repeat(bins[i], len(aux_YD)), 'k')
-    plt.plot(aux_YD, '*b', label='Valor Deseado')
-    plt.plot(aux_YK[0], '*r', label='Respuesta de Red')
+    plt.plot(aux_YD, '*b', label='Real Value')
+    plt.plot(aux_YK[0], '*r', label='Model Prediction')
     plt.legend(loc='best')
-    plt.title('Deseado vs Respuesta')
+    plt.title('Real vs Prediction')
 
 
   @staticmethod
   def probas(true_values:np.ndarray, 
              predicted_values:np.ndarray, 
              names:np.ndarray) -> pd.core.frame.DataFrame:
+    """
+    This function gives the probability of a predictions really
+    belonging to the predicted class
+      Parameters:
+        true_values -> array with the real values
+        predicted_values -> array with the model predictions
+      Returns:
+        table -> a table with the real class, predicted class, and probability
+    """
 
-      n_partitions = len(np.unique(true_values)) #Definir la cantidad de particiones
-      bins = np.linspace(0, 1, n_partitions + 1) #Crear los límites de los rangos
-      arr_class = np.digitize(predicted_values, bins).astype(float) #Clasificar los datos en los rangos
-      arr_class = (arr_class - 1) / (n_partitions - 1) #Asignar la clase correspondiente
+    n_partitions = len(np.unique(true_values)) #Define the number of partitions
+    bins = np.linspace(0, 1, n_partitions + 1) #Create ranges' limits
+    arr_class = np.digitize(predicted_values, bins).astype(float) #Classify the data in the ranges
+    arr_class = (arr_class - 1) / (n_partitions - 1) #Asign the corresponding class
 
-      real = true_values.reshape((true_values.shape[0], 1))
-      clasif = arr_class.reshape((arr_class.shape[0], 1))
-      probas = 1-np.abs(clasif-predicted_values)
-      tabla = pd.DataFrame(np.hstack((clasif, probas, real)), 
-                          columns=['Clasificación Red', 'Probabilidad', 'Clasificación Real'])
+    real = true_values.reshape((true_values.shape[0], 1))
+    clasif = arr_class.reshape((arr_class.shape[0], 1))
+    probas = 1-np.abs(clasif-predicted_values)
+    tabla = pd.DataFrame(np.hstack((clasif, probas, real)), 
+                        columns=['Model Classification', 'Probability', 'Real Classification'])
 
-      valores_unicos = tabla['Clasificación Real'].unique()
-      nombres = names
-      mapeo = {valor: nombre for valor, nombre in zip(valores_unicos, nombres)}
-      tabla['Clasificación Red'] = tabla['Clasificación Red'].replace(mapeo)
-      tabla['Clasificación Real'] = tabla['Clasificación Real'].replace(mapeo)
+    valores_unicos = tabla['Real Classification'].unique()
+    nombres = names
+    mapeo = {valor: nombre for valor, nombre in zip(valores_unicos, nombres)}
+    tabla['Model Classification'] = tabla['Model Classification'].replace(mapeo)
+    tabla['Real Classification'] = tabla['Real Classification'].replace(mapeo)
 
-      return tabla
+    return tabla
